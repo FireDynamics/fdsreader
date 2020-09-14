31c8e9965eacbea20dbfc54c02a391e9e187acfe
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2020-09-14 12:37:07 +0200

    Replaced edx theme by rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 from fds._version import __version__
-import edx_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -31,7 +30,7 @@ release = str(__version__)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['edx_theme']
+extensions = ["sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 #   templates_path = ['_templates']
@@ -47,8 +46,7 @@ exclude_patterns = ['Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'edx_theme'
-html_theme_path = [edx_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
