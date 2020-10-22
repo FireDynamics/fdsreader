d4027528deed7639459b9b61b0dcd2358696fb1e
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2020-10-22 12:32:57 +0200

    Added docstrings to slice.py and fixed mean functionality

--- fds/slcf/slice.py
+++ fds/slcf/slice.py
@@ -1,7 +1,6 @@
 import logging
 import os
-from typing import List, Tuple, Union, Iterator
-from typing_extensions import Literal
+from typing import List, Dict
 
 import numpy as np
 import numpy.lib.mixins
@@ -20,7 +19,6 @@ else:
     DTYPE_TIME = np.dtype(FDS_DATA_TYPE_FLOAT)
     DTYPE_STRIDE_RAW = "({})" + FDS_DATA_TYPE_FLOAT
 
-
 _HANDLED_FUNCTIONS = {}
 
 
@@ -28,32 +26,42 @@ def implements(np_function):
     """
     Decorator to register an __array_function__ implementation for Slices.
     """
+
     def decorator(func):
         _HANDLED_FUNCTIONS[np_function] = func
         return func
+
     return decorator
 
 
 class Slice(numpy.lib.mixins.NDArrayOperatorsMixin):
     """
-
+    Slice file data container including metadata. Consists of multiple subslices, one for each mesh
+    the slice cuts through.
+    :ivar root_path: Path to the directory containing all slice files.
+    :ivar quantities: List with quantity objects containing information about the quantities
+    calculated for this slice with the corresponding label and unit
+    :ivar cell_centered: Indicates whether centered positioning for data is used
+    :ivar _subslices: List of all subslices this slice consists of
     """
+
     def __init__(self, root_path: str, cell_centered: bool):
         self.root_path = root_path
-        self.quantities = list()
+        self.quantities: List[Quantity] = list()
 
         self.cell_centered = cell_centered
-        self._subslices = list()
+        self._subslices: List[_SubSlice] = list()
 
     def _add_subslice(self, filename: str, quantity: str, label: str, unit: str, extent: Extent,
                       mesh_id: int):
         """
-        :param filename:
-        :param quantity:
-        :param label:
-        :param unit:
-        :param extent:
-        :param mesh_id:
+        Adds another subslice to the slice.
+        :param filename: Name of the slice file
+        :param quantity: Quantity of the data
+        :param label: Quantity label
+        :param unit: Quantity unit
+        :param extent: Extent object containing 3-dimensional extent information
+        :param mesh_id: Id of the mesh the subslice cuts through
         """
         self.quantities.append(Quantity(quantity, label, unit))
         for subslice in self._subslices:
@@ -62,38 +70,60 @@ class Slice(numpy.lib.mixins.NDArrayOperatorsMixin):
                 break
         self._subslices.append(_SubSlice(filename, extent, quantity, mesh_id))
 
+    def mean(self, quantity: str = None):
+        """
+        Calculates the mean over the whole slice.
+        :param quantity: The quantity for which the mean should be calculated. If not provided, the
+        first found quantity will be used
+        :returns: The calculated mean value.
+        """
+        if quantity is None:
+            quantity = self.quantities[0].quantity
+        mean_sum = 0
+        for subsclice in self._subslices:
+            mean_sum += np.mean(subsclice.get_data(quantity, self.root_path, self.cell_centered))
+        return mean_sum / len(self._subslices)
+
     def __array__(self):
         """
-
+        Method that will be called by numpy when trying to convert the object to a numpy ndarray.
         """
-        raise UserWarning("Slices can not be converted to numpy arrays, but they support all typical numpy operations"
-                          "such as np.multiply.")
+        raise UserWarning(
+            "Slices can not be converted to numpy arrays, but they support all typical numpy"
+            "operations such as np.multiply. If a 'global' array containg all subslices is"
+            "required, please request this functionality by submitting an issue on Github.")
 
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
         """
-        
+        Method that will be called by numpy when using a ufunction with a Slice as input.
+        :returns: A new slice on which the ufunc has been applied.
         """
         if method != "__call__":
-            logging.warning("The %s method has been used which is not explicitly implemented. Correctness of results is"
-                            " not guaranteed. If you require this feature to be implemented please submit an issue"
-                            " on github where you explain your use case.", method)
+            logging.warning(
+                "The %s method has been used which is not explicitly implemented. Correctness of"
+                " results is not guaranteed. If you require this feature to be implemented please"
+                " submit an issue on Github where you explain your use case.", method)
         input_list = list(inputs)
-        for i, input in enumerate(inputs):
-            if isinstance(input, self.__class__):
+        for i, inp in enumerate(inputs):
+            if isinstance(inp, self.__class__):
                 del input_list[i]
 
         new_slice = Slice(self.root_path, self.cell_centered)
         for i, subslice in enumerate(self._subslices):
             q = self.quantities[i]
-            new_slice._add_subslice(subslice.file_names[q.quantity], q.quantity, q.label,
+            new_slice._add_subslice(subslice.file_names[q.quantity],
+                                    q.quantity, q.label,
                                     q.unit, subslice.extent, subslice.mesh_id)
             new_slice._subslices[-1]._data[q.quantity] = ufunc(
-                subslice.get_data(q.quantity, self.root_path, self.cell_centered), input_list[0], **kwargs)
+                subslice.get_data(q.quantity, self.root_path, self.cell_centered),
+                input_list[0],
+                **kwargs)
         return new_slice
 
     def __array_function__(self, func, types, args, kwargs):
         """
-
+        Method that will be called by numpy when using an array function with a Slice as input.
+        :returns: The output of the array function.
         """
         if func not in _HANDLED_FUNCTIONS:
             return NotImplemented
@@ -106,42 +136,49 @@ class Slice(numpy.lib.mixins.NDArrayOperatorsMixin):
 
 class _SubSlice:
     """
-
+    Part of a slice that cuts through a single mesh.
+    :ivar mesh_id: Id of the mesh the subslice cuts through
+    :ivar extent: Extent object containing 3-dimensional extent information
+    :ivar file_names: File names for the corresponding slice file depending on the quantity.
+    :ivar _data: Dictionary that maps quantity to data.
     """
+
     def __init__(self, filename: str, extent: Extent, quantity: str, mesh_id: int):
         self.mesh_id = mesh_id
         self.extent = extent
 
         self.file_names = {quantity: filename}
-        self._data = dict()
+        self._data: Dict[str, np.ndarray] = dict()
 
     def get_data(self, quantity: str, root_path: str, cell_centered: bool):
         """
-
+        Method to lazy load the slice's data.
+        :param quantity: Quantity of the data
+        :param root_path: Path to the directory containing all slice files.
+        :param cell_centered: Indicates whether centered positioning for data is used.
         """
         if quantity not in self._data:
             file_path = os.path.join(root_path, self.file_names[quantity])
             dtype_float = np.dtype(FDS_DATA_TYPE_FLOAT)
-            N = self.extent.size(cell_centered=cell_centered)
-            stride = np.dtype(DTYPE_STRIDE_RAW.format(N)).itemsize
+            n = self.extent.size(cell_centered=cell_centered)
+            stride = np.dtype(DTYPE_STRIDE_RAW.format(n)).itemsize
             offset = 3 * DTYPE_HEADER.itemsize + DTYPE_INDEX.itemsize
 
             t_n = (os.stat(file_path).st_size - offset) // stride
 
-            self._data[quantity] = np.empty((t_n, self.extent.x, self.extent.y, self.extent.z), dtype=dtype_float)
+            self._data[quantity] = np.empty((t_n, self.extent.x, self.extent.y, self.extent.z),
+                                            dtype=dtype_float)
 
             with open(file_path, 'r') as infile:
                 for t in range(t_n):
                     infile.seek(offset + t * stride)
-                    slice_data = np.fromfile(infile, dtype=dtype_float, count=N)
-                    self._data[quantity][t, :] = slice_data.reshape((self.extent.x, self.extent.y, self.extent.z))
+                    slice_data = np.fromfile(infile, dtype=dtype_float, count=n)
+                    self._data[quantity][t, :] = slice_data.reshape(
+                        (self.extent.x, self.extent.y, self.extent.z))
         return self._data[quantity]
 
 
 # __array_function__ implementations
 @implements(np.mean)
 def mean(slc: Slice):
-    mean = 0
-    for subsclice in slc._subslices:
-        mean += np.mean(subsclice.get_data())
-    return mean / len(slc._subslices)
+    return [slc.mean(q.quantity) for q in slc.quantities]
