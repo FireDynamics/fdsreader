9d0fc9896691178facca59d329e8ea7ba0a304b0
Jan Vogelsang (via Travis CI) <j.vogelsang@fz-juelich.de>
2020-11-11 21:45:39 +0000

    Deploy JanVogelsang/fdsreader to github.com/JanVogelsang/fdsreader.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '[fdsreader, version 0.2.0dev1]',
+    VERSION: '[fdsreader, version 0.3.0dev1]',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
