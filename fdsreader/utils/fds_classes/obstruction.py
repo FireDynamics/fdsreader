f73889a7b03b11966571766402df42f1b51f358d
Jan Vogelsang <janv1999@outlook.com>
2020-11-30 16:04:47 +0100

    Fixed api docs

--- fdsreader/utils/fds_classes/obstruction.py
+++ fdsreader/utils/fds_classes/obstruction.py
@@ -13,14 +13,14 @@ class Obstruction:
     :ivar side_surfaces: Tuple of six surfaces for each side of the cuboid.
     :ivar bound_indices: Indices used to define obstruction bounds in terms of mesh locations.
     :ivar color_index: Type of coloring used to color obstruction.
-        -1 - default color
-        -2 - invisible
-        -3 - use red, green, blue and alpha (rgba attribute)
-        n>0 - use n’th color table entry
+     \n-1 - default color
+     \n-2 - invisible
+     \n-3 - use red, green, blue and alpha (rgba attribute)
+     \nn>0 - use n’th color table entry
     :ivar block_type: Defines how the obstruction is drawn.
-        -1 - use surface to obtain blocktype
-        0 - regular block
-        2 - outline
+     \n-1 - use surface to obtain blocktype
+     \n0 - regular block
+     \n2 - outline
     :ivar texture_origin: Origin position of the texture provided by the surface. When the texture does have a pattern,
         for example windows or bricks, the texture_origin specifies where the pattern should begin.
     :ivar rgba: Optional color of the obstruction in form of a 4-element tuple (ranging from 0.0 to 1.0).
