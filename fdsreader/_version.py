505d8130d1f6994d5b54abbdc399375a8d79dee5
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2021-02-22 15:29:29 +0100

    Fixed path issue in setup.py

--- fdsreader/_version.py
+++ fdsreader/_version.py
@@ -7,5 +7,5 @@ Provides fdsreader version information.
 
 from incremental import Version
 
-__version__ = Version('fdsreader', 0, 6, 4)
+__version__ = Version('fdsreader', 0, 6, 5)
 __all__ = ["__version__"]
