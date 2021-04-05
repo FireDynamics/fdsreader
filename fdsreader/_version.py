e09698443e50420f2a109b48254920fd636fa6eb
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2021-04-05 09:55:44 +0200

    Relaxed requirements

--- fdsreader/_version.py
+++ fdsreader/_version.py
@@ -7,5 +7,5 @@ Provides fdsreader version information.
 
 from incremental import Version
 
-__version__ = Version('fdsreader', 0, 9, 12)
+__version__ = Version("fdsreader", 0, 9, 13)
 __all__ = ["__version__"]
