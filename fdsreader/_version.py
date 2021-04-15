0ab500bcff03e81ab46a60ca8df6b56bdb5097dd
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2021-04-15 10:10:47 +0200

    Incremented version

--- fdsreader/_version.py
+++ fdsreader/_version.py
@@ -7,5 +7,5 @@ Provides fdsreader version information.
 
 from incremental import Version
 
-__version__ = Version('fdsreader', 0, 9, 20)
+__version__ = Version('fdsreader', 0, 9, 21)
 __all__ = ["__version__"]
