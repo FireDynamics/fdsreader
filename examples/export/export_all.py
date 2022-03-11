612e858d00d4fca1e9f052be615fbeed49049763
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2022-03-11 17:04:23 +0100

    Trying to fix travis CI

--- examples/export/export_all.py
+++ examples/export/export_all.py
@@ -1,14 +1,14 @@
 import os
-from fdsreader import Simulation
-import fdsreader.export as exp
+import fdsreader as fds
+import fdsreader.export
 
 
 def main():
     base_path = "C:\\Users\\janv1\\Documents\\UnrealProjects\\VRSmokeVis"
     case = "Apartment"
-    sim = Simulation(os.path.join(base_path, "fds_data", case))
+    sim = fds.Simulation(os.path.join(base_path, "fds_data", case))
 
-    print(exp.export_sim(sim, os.path.join(base_path, "fds_post", case), ordering='F'))
+    print(fds.export.export_sim(sim, os.path.join(base_path, "fds_post", case), ordering='F'))
 
 
 if __name__ == "__main__":
