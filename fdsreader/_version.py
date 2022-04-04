f973a9ec105a60b73d896b2824f2ec9f9e461054
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2022-04-04 16:40:44 +0200

    Setting version to 1.5.5 to upload to pypi

--- fdsreader/_version.py
+++ fdsreader/_version.py
@@ -7,5 +7,5 @@ Provides fdsreader version information.
 
 from incremental import Version
 
-__version__ = Version("fdsreader", 1, 5, 3)
+__version__ = Version("fdsreader", 1, 5, 5)
 __all__ = ["__version__"]
