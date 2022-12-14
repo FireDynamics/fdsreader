1ce02571f988b8cf1ccc4f86075a4cb6c7977c12
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2022-12-14 20:31:24 +0000

    Deploy FireDynamics/fdsreader to github.com/FireDynamics/fdsreader.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '[fdsreader, version 1.9.2]',
+    VERSION: '[fdsreader, version 1.9.3]',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
