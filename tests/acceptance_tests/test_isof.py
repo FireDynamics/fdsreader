85c0b6db807561a918cfec23541859ff1a566bf2
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2024-03-14 18:43:13 +0100

    Fixed isof test

--- tests/acceptance_tests/test_isof.py
+++ tests/acceptance_tests/test_isof.py
@@ -7,4 +7,4 @@ def test_isof():
     vertices, triangles, _ = isosurface.to_global(len(isosurface.times) - 1)
 
     assert abs(vertices[-1][0] - 2.80595016) < 1e-6 and abs(vertices[-1][1] - 0.1) < 1e-6 and abs(vertices[-1][2] - 1.83954549) < 1e-6
-    assert abs(triangles[-1][-1][0] - 4625) < 1e-6 and abs(triangles[-1][-1][0] - 4627) < 1e-6 and abs(triangles[-1][-1][0] - 4708) < 1e-6
+    assert abs(triangles[-1][-1][0] - 4625) < 1e-6 and abs(triangles[-1][-1][1] - 4627) < 1e-6 and abs(triangles[-1][-1][2] - 4708) < 1e-6
