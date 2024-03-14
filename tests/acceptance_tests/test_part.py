4c5c46784bf16e91a3b6f4efce9043964c1d34d4
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2024-03-14 19:47:28 +0100

    Fixed part test

--- tests/acceptance_tests/test_part.py
+++ tests/acceptance_tests/test_part.py
@@ -7,5 +7,5 @@ def test_part():
     position = particles.positions[-1][-1]
     color_data = particles.data["PARTICLE DIAMETER"]
 
-    assert abs(position[0] - 0.04036335) < 1e-6 and abs(position[1] - 0.05389348) < 1e-6 and abs(position[2] - 13.596354) < 1e-6
+    assert abs(position[0] + 0.04036335) < 1e-6 and abs(position[1] - 0.05389348) < 1e-6 and abs(position[2] - 13.596354) < 1e-6
     assert abs(color_data[-1][-1] - 2.2600818) < 1e-6
