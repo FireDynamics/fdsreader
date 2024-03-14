80174e1d3ac5f3644af58f7f483bfc94553ea40e
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2024-03-14 19:45:33 +0100

    Fixed part test

--- tests/acceptance_tests/test_part.py
+++ tests/acceptance_tests/test_part.py
@@ -4,7 +4,7 @@ from fdsreader import Simulation
 def test_part():
     sim = Simulation("./part_data")
     particles = sim.particles["WATER PARTICLES"]
-    position = particles.positions[-1]
+    position = particles.positions[-1][-1]
     color_data = particles.data["PARTICLE DIAMETER"]
 
     assert abs(position[0] - 0.04036335) < 1e-6 and abs(position[1] - 0.05389348) < 1e-6 and abs(position[2] - 13.596354) < 1e-6
--- tests/acceptance_tests/test_slcf.py
+++ tests/acceptance_tests/test_slcf.py
@@ -5,4 +5,4 @@ def test_slcf():
     sim = Simulation("./steckler_data")
     data, coordinates = sim.slices[0].to_global(masked=True, return_coordinates=True)
     assert abs(data[-1, -1, -1] - 33.311744689941406) < 1e-6
-    assert abs(coordinates['x'][0] - 0.) < 1e-6 and abs(coordinates['x'] - 3.6) < 1e-6
+    assert abs(coordinates['x'][0] - 0.) < 1e-6 and abs(coordinates['x'][-1] - 3.6) < 1e-6
