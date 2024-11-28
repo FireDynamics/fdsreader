7cf968dcadfbe5216660c18e55e0b3bbe9869ee1
Lukas <lu-kas@users.noreply.github.com>
2024-11-28 13:54:03 +0100

    Fix for reading BNDF times

--- fdsreader/simulation.py
+++ fdsreader/simulation.py
@@ -619,25 +619,8 @@ class Simulation:
         patches = dict()
         mesh_patches = dict()
 
-        if os.path.exists(file_path + ".bnd"):
-            times = list()
-            lower_bounds = list()
-            upper_bounds = list()
-            with open(file_path + ".bnd", 'r') as bnd_file:
-                for line in bnd_file:
-                    splits = line.split()
-                    times.append(float(splits[0]))
-                    lower_bounds.append(float(splits[1]))
-                    upper_bounds.append(float(splits[2]))
-            times = np.array(times)
-            lower_bounds = np.array(lower_bounds, dtype=np.float32)
-            upper_bounds = np.array(upper_bounds, dtype=np.float32)
-            n_t = times.shape[0]
-        else:
-            times = None
-            n_t = -1
-            lower_bounds = np.array([0.0], dtype=np.float32)
-            upper_bounds = np.array([np.float32(-1e33)], dtype=np.float32)
+        lower_bounds = np.array([np.float32(+1e33)], dtype=np.float32)
+        upper_bounds = np.array([np.float32(-1e33)], dtype=np.float32)
 
         with open(file_path, 'rb') as infile:
             # Offset of the binary file to the end of the file header.
@@ -650,6 +633,24 @@ class Simulation:
             patch_infos = fdtype.read(infile, dtype_patches, n_patches)
             offset += fdtype.INT.itemsize + dtype_patches.itemsize * n_patches
             patch_offset = fdtype.FLOAT.itemsize
+
+            # Determine the size of the data block for all patches
+            patches_data_bytes = 0
+            for patch_info in patch_infos:
+                patch_info = patch_info[0]
+                extent, dimension = self._indices_to_extent(patch_info[:6], mesh)
+                patches_data_bytes += fdtype.new((('f', str(dimension.shape(cell_centered=False))),)).itemsize
+
+            # Time info 
+            time_bytes = fdtype.FLOAT.itemsize
+            n_t = (os.stat(file_path).st_size - offset) // (time_bytes + patches_data_bytes)
+
+            times = list()
+            for _ in range(n_t):
+                time = fdtype.read(infile, fdtype.FLOAT, 1)[0][0][0]
+                times.append(time)
+                offset += time_bytes + patches_data_bytes
+                infile.seek(offset)
 
             for patch_info in patch_infos:
                 patch_info = patch_info[0]
