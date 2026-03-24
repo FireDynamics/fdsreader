e9eca59d5c459c9c4068c9643dc5ab5c749850dc
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2026-03-24 10:11:07 +0100

    Add test to reproduce bug

--- tests/acceptance_tests/test_devc.py
+++ tests/acceptance_tests/test_devc.py
@@ -1,6 +1,15 @@
+import os
 from fdsreader import Simulation
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_devc():
-    sim = Simulation("./steckler_data")
+    sim = Simulation(os.path.join(TEST_DIR, "../cases/steckler_data"))
     assert abs(sim.devices["TC_Door"][0].data - 23.58) < 1e-6
+
+
+def test_clear_cache_with_line_devices():
+    """Test that clear_cache works with line DEVC devices (issue #104)."""
+    sim = Simulation(os.path.join(TEST_DIR, "../cases/devc_data"))
+    sim.clear_cache()
